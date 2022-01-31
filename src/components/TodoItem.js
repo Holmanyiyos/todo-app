2ac564ff8c329c2f35c7d0ad17abba0f319b5cee
Holman Plazas <holmanyiyos@gmail.com>
2022-01-31 14:36:24 -0500

    dia 1 cambio de estilos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,27 +3,17 @@ import "../css/TodoItem.css"
 
 function TodoItem(props){
     const handelClick= (e)=>{
-        if (e.target.nodeName === "I") {
-            
-        }else{
+        if (e.target.className.includes("edit-item")) {
+            console.log(e.target.className)
             props.editTodo(props.id)
             props.setOpenModal(prevState => !prevState)
             props.whoModal("edit")
+        }else{
+
         }
     }
         return(
-        <li className={`todo-item ${props.priority} ${props.complete ? 'completed': ""}`} onClick={handelClick}>
-        <i 
-            className={`${props.complete ? 'fas fa-check-circle': 'far fa-circle'} check-item`}
-            onClick={props.onComplete}
-        >
-        </i>
-        <p className="text">{props.text}</p>
-        <i 
-            className="far fa-trash-alt close-item"
-            onClick={props.onDelete}
-        >
-        </i>
+        <li className={`todo-item ${props.priority} ${props.complete ? 'completed': ""}`}>
         <div className="footer-item">
             {props.priority === "a" && 
             <div className="priority-container">
@@ -42,6 +32,21 @@ function TodoItem(props){
             </div>}
     
         </div>
+        <i 
+            className={`${props.complete ? 'fas fa-check-circle': 'far fa-circle'} check-item`}
+            onClick={props.onComplete}
+        >
+        </i>
+        <p className="text">{props.text}</p>
+        <i 
+            className="far fa-trash-alt close-item"
+            onClick={props.onDelete}
+        >
+        </i>
+
+        <i className="fas fa-edit edit-item"
+        onClick={handelClick}
+        ></i>
     </li>
        )
 }
