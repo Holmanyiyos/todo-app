1dec64ad4a9bd8aa7983fded4bf9ee9313ef8fec
Holman Plazas <holmanyiyos@gmail.com>
2022-02-18 09:38:23 -0500

    Updates

---
+++ precache-manifest.d8ebc1e2d75aa3130d9cc98ad18dc805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19842bc8391a83d261c09948eb05b767",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "4caaaaf28acde60355d0",
+    "url": "/todo-app/static/css/main.99b78e0c.chunk.css"
+  },
+  {
+    "revision": "7f5f12e9130a15bcf687",
+    "url": "/todo-app/static/js/2.4987db48.chunk.js"
+  },
+  {
+    "revision": "4caaaaf28acde60355d0",
+    "url": "/todo-app/static/js/main.d1428ecd.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
